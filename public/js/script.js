ef4bf8c1dfba2fee20ee686e00a0b30c1b9fd8e4
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2022-08-22 09:20:03 +0200

    förbättrar js och timern

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
-var timer = document.getElementById("quiz_timer").textContent;
+var timerLength = document.getElementById("quiz_timer").textContent;
+var timeText = document.getElementById("visual_quiz_timer")
 
-if (typeof(timer) != typeof(null)) {
-    setTimeout(timeout, parseInt(timer)*1000);
-    var time = setInterval(function() {
-        document.getElementById("visual_quiz_timer").textContent = document.getElementById("visual_quiz_timer").textContent - 1;
-    }, 1000);
-}
+setTimeout(timeout, parseInt(timerLength)*1000);
+
+var time = setInterval(function() {
+    timeText.textContent = timeText.textContent - 1;
+}, 1000);
 
 function timeout() {
     window.location.href = "/svar";
