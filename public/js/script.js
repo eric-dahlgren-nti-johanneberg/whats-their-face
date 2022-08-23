2f3f152bbfc700752abac446e1e786efffe46409
jack-blomquist-nti-johanneberg <jack.blomquist@elev.ga.ntig.se>
2022-08-23 11:37:28 +0200

    js sak

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,22 @@
 if (window.location.pathname == "/") {
   var time_value = document.getElementById("timer");
   var display_time_value = document.getElementById("display_timer");
-  let names = document.getElementById("names");
   let slider = document.getElementById("option_select");
-  let maxLabel = document.getElementById("max");
   var display_choice_value = document.getElementById("options");
-
-  time_value.oninput = function () {
-    display_time_value.innerHTML =
-      "Time per person: " + this.value + " seconds";
-  };
+  var rounds = document.getElementById("rounds");
+  var display_rounds = document.getElementById("display_rounds");
 
   slider.oninput = function () {
     display_choice_value.innerHTML = "Number of choices: " + this.value;
   };
-  names.oninput = () => {
-    const values = names.value.split("\n").length;
-    slider.max = values;
-    maxLabel.textContent = values;
+
+  time_value.oninput = function () {
+    display_time_value.innerHTML = "Time per person: " + this.value + " seconds";
   };
 
+  rounds.oninput = function() {
+    display_rounds.innerHTML = "Number of rounds:" + this.value;
+  };
 }
 
 if (window.location.pathname == "/quiz") {
