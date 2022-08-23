ace5b57f8cf1ea45363dbb6ad88278da4d0648fd
Eric Dahlgren <eric.dahlgren@elev.ga.ntig.se>
2022-08-23 09:52:29 +0200

    range fungerar

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,35 @@
+if (window.location.pathname == "/") {
+  var time_value = document.getElementById("timer");
+  var display_time_value = document.getElementById("display_timer");
+  let names = document.getElementById("names");
+  let slider = document.getElementById("option_select");
+  let maxLabel = document.getElementById("max");
 
-if (window.location.pathname == "/"){
-    var time_value = document.getElementById("timer");
-    var display_time_value = document.getElementById("display_timer");
+  names.oninput = () => {
+    const values = names.value.split("\n").length;
+    slider.max = values;
+    maxLabel.textContent = values;
+  };
 
-    time_value.oninput = function(){
-        display_time_value.innerHTML = "Time per person: " + this.value;
-    }
+  time_value.oninput = function () {
+    display_time_value.innerHTML = "Time per person: " + this.value;
+  };
 }
 
-if (window.location.pathname == "/quiz"){
-    var time = document.getElementById("visual_quiz_timer");
-    var timerLength = time.innerHTML;
+if (window.location.pathname == "/quiz") {
+  var time = document.getElementById("visual_quiz_timer");
+  var timerLength = time.innerHTML;
 
-    setTimeout(timeout, parseInt(timerLength)*1000);
+  setTimeout(timeout, parseInt(timerLength) * 1000);
 
-    var timeText = setInterval(function() {
-        time.innerHTML = time.innerHTML - 1;
-        if(time.innerHTML < 6){
-            time.style.color = "red";
-        }
-    }, 1000);
+  var timeText = setInterval(function () {
+    time.innerHTML = time.innerHTML - 1;
+    if (time.innerHTML < 6) {
+      time.style.color = "red";
+    }
+  }, 1000);
 }
 
 function timeout() {
-    window.location.href = "/timeout";
+  window.location.href = "/timeout";
 }
-
